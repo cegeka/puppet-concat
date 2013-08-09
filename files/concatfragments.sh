puppetlabs/puppetlabs-concat

commit 3d60c9ef6162425c0bf9eb2004d9da375965efc9

Refactor loop to use for-do instead of while-read.
This for loop around find removes the need for the read statement.
The read statement isn't portable to solaris, because Read has no -r option
in /bin/sh on solaris. This should resolve Issue #45.

files/concatfragments.sh
@@ -112,9 +112,14 @@ else
 fi
 
 # find all the files in the fragments directory, sort them numerically and concat to fragments.concat in the working dir
-find fragments/ -type f -follow | LANG=C sort ${SORTARG} | while read -r fragfile; do
-	cat "$fragfile" >> "fragments.concat"
+IFS_BACKUP=$IFS
+IFS='
+'
+for fragfile in `find fragments/ -type f -follow | LANG=C sort ${SORTARG}`
+do
+    cat $fragfile >> concat.fragment
 done
+IFS=$IFS_BACKUP
 
 if [ x${TEST} = "x" ]; then
 	# This is a real run, copy the file to outfile

cegeka/puppet-concat

commit 27260a63ab248154028b444ceca2cc443dba49c9

Update concatfragments.sh ... fix broken behavior introduced in 3d60c9ef6162425c0bf9eb2004d9da375965efc9

files/concatfragments.sh
@@ -117,7 +117,7 @@ IFS='
 '
 for fragfile in `find fragments/ -type f -follow | LANG=C sort ${SORTARG}`
 do
-    cat $fragfile >> concat.fragment
+    cat $fragfile >> "fragments.concat"
 done
 IFS=$IFS_BACKUP
 