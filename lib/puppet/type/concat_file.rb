puppetlabs/puppetlabs-concat

commit dd88b1a4eee853cfff60b99096c05ce85a9d556e

(MODULES-3463) Properly passes metaparams to generated resource

lib/puppet/type/concat_file.rb
@@ -193,6 +193,15 @@ Puppet::Type.newtype(:concat_file) do
       end
     end
 
+    metaparams = Puppet::Type.metaparams
+    excluded_metaparams = [ :before, :notify, :require, :subscribe, :tag ]
+
+    metaparams.reject! { |param| excluded_metaparams.include? param }
+
+    metaparams.each do |metaparam|
+      file_opts[metaparam] = self[metaparam] if self[metaparam]
+    end
+
     [Puppet::Type.type(:file).new(file_opts)]
   end
 

spec/acceptance/concat_spec.rb
@@ -195,5 +195,34 @@ describe 'basic concat test' do
         its(:content) { should match '1' }
       end
     end
+    context 'noop properly' do
+      before(:all) do
+        pp = <<-EOS
+        file { '#{basedir}':
+          ensure => directory,
+        }
+        EOS
+        apply_manifest(pp)
+      end
+      pp="
+        concat { 'file':
+          ensure => present,
+          path   => '#{basedir}/file',
+          mode   => '0644',
+          noop   => true,
+        }
+        concat::fragment { '1':
+          target  => 'file',
+          content => '1',
+          order   => '01',
+        }
+      "
+
+      it_behaves_like 'successfully_applied', pp
+
+      describe file("#{basedir}/file") do
+        it { should_not be_file }
+      end
+    end
   end
 end

cegeka/puppet-concat

commit 4515435a8e79d74b2d7998a39fbdfd87c8abe011

Do not modify metaparams in place
This can have unexpected side effects and break other code that relies
on it. This takes a safer approach by iterating over it.

Fixes: dd88b1a4eee853cfff60b99096c05ce85a9d556e

lib/puppet/type/concat_file.rb
@@ -341,13 +341,12 @@ Puppet::Type.newtype(:concat_file) do
       file_opts[param] = self[param] unless self[param].nil?
     end
 
-    metaparams = Puppet::Type.metaparams
     excluded_metaparams = [:before, :notify, :require, :subscribe, :tag]
 
-    metaparams.reject! { |param| excluded_metaparams.include? param }
-
-    metaparams.each do |metaparam|
-      file_opts[metaparam] = self[metaparam] unless self[metaparam].nil?
+    Puppet::Type.metaparams.each do |metaparam|
+      unless self[metaparam].nil? || excluded_metaparams.include?(metaparam)
+        file_opts[metaparam] = self[metaparam]
+      end
     end
 
     [Puppet::Type.type(:file).new(file_opts)]